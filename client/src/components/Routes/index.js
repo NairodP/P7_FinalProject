42c0efa94f8fa2f5542b0284b0c34f8c7846e64b
Dorian <dorianpernot2@gmail.com>
2022-10-28 16:39:22 +0200

    rectif précédent commit

--- client/src/components/Routes/index.js
+++ client/src/components/Routes/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../../pages/Home";
 import Connexion from "../../pages/Log";
-// import { UidContext } from "../AppContext";
 import Profil from "../Profil";
-// import PubliUploader from "../PubliUploader";
 
 const RoutesApp = () => {
 
